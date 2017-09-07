361ccdc322a30ba0689377005d32440b5e0a2225
Veronika <veronika@ranta.com>
2017-09-07 09:41:36 +0200

    Add project files.

---
+++ Uppgifter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Uppgifter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
